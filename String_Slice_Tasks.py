cfab7b961368775b3354112a50dac2e4477b3bef
Jyoti <100664142+Jyotixyz@users.noreply.github.com>
2022-03-14 23:21:11 +0530

    Add files via upload
    13th March task completion.

---
+++ String_Slice_Tasks.py
@@ -0,0 +1,80 @@
+Python 3.10.2 (v3.10.2:a58ebcc701, Jan 13 2022, 14:50:16) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+
+"Information_Technology"	
+#[-2:0:-1]	golonhceT_noitamrofn
+#[5:-5:5]	mnh
+#[-5:5:-5]	oTt
+#[5:-5:-5]	empty
+#[3:9:-1]	empty
+#[9:-10:-2]	empty
+#[2:-4:-4]	empty
+#[-4:2:-2]	lncTniar
+#[0:-1:-3]	empty
+#[-3:3:6]	empty
+#[::-3]	ylhToaoI
+#[::-2]	yoohe_otmon
+#[2:-2:-2]	empty
+#[2:-2:2]	frainTcnl
+#[5:-5:5]	mnh
+#[3:-3:3]	oaoThl
+#[::2]	IfrainTcnlg
+#[::-5]	yn_an
+	
+"python_is_using_interpreter"	
+	
+#[-2:0:-1]	eterpretni_gnisu_si_nohty
+#[5:-5:5]	nu_r
+#[-5:5:-5]	rnii
+#[5:-5:-5]	empty
+#[3:9:-1]	empty
+#[9:-10:-2]	empty
+#[2:-4:-4]	empty
+#[-4:2:-2]	epen_ns_inh
+#[0:-1:-3]	empty
+#[-3:3:6]	empty
+#[::-3]	rerngssnt
+#[::-2]	rtrrtigius_otp
+#[2:-2:-2]	empty
+#[2:-2:2]	to_suigitrrt
+#[5:-5:5]	nu_r
+#[3:-3:3]	h__i_tp
+#[::2]	pto_suigitrrtr
+#[::-5]	rpis_y
+	
+"computer science"	
+print(a[-10:10:3])	es
+print(a[-5:-1:1])	ienc
+print(a[-8:-2:10])	One space
+print(a[5::3])	t ic
+print(a[:5:2])	cmu
+print(a[10:14:2])	ce
+print(a[-10:-3:3])	ese
+print(a[5:12:5])	tc
+print(a[3:-3:3])	pese
+print(a[-5:-3:20])	i
+	
+"Information_technology"	
+print(a[-10:10:3])	Empty
+print(a[-5:-1:1])	olog
+print(a[-8:-2:10])	c
+print(a[5::3])	mi_cog
+print(a[:5:2])	Ifr
+print(a[10:14:2])	nt
+print(a[-10:-3:3])	thl
+print(a[5:12:5])	mn
+print(a[3:-3:3])	oaothl
+print(a[-5:-3:20])	o
+	
+"Google_chrome_browser"	
+print(a[-10:10:3])	Empty
+print(a[-5:-1:1])	owse
+print(a[-8:-2:10])	_
+print(a[5::3])	ehmbwr
+print(a[:5:2])	Gol
+print(a[10:14:2])	oe
+print(a[-10:-3:3])	mbw
+print(a[5:12:5])	eo
+print(a[3:-3:3])	g_rer
+print(a[-5:-3:20])	o
+	
